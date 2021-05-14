7e50c15223624fe289b59685063d8474ee449494
Mahdi Yari <m.yary15@gmail.com>
2021-05-15 01:24:54 +0430

    Update config.example.js

--- back-end/config.example.js
+++ back-end/config.example.js
@@ -3,7 +3,8 @@ const config = {
   dbUser: 'root',
   dbPassword: 'mysql',
   dbHost: '127.0.0.1',
-  dbPort: 3306
+  dbPort: 3306,
+  node: 'https://api.ha.deathwing.me'
 }
 
 module.exports = config
